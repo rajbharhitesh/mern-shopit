f62d464501bfbb636e09cb86205f02b872947887
Hitesh <h08rajbhar@gmail.com>
2024-01-27 16:42:44 +0530

    initial basic setup with database connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+const app = express();
+
+// config
+dotenv.config({
+  path: 'backend/config/config.env',
+});
+
+const PORT = process.env.PORT || 5000;
+
+// connect database
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on PORT:${PORT}`.bgBlue
+      .black.underline.bold
+  );
+});
