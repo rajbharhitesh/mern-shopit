60c341e1c1c7f29d5c04302f76514ebe6aef0121
Hitesh <h08rajbhar@gmail.com>
2024-01-28 14:48:13 +0530

    handling errors in production and development

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -4,7 +4,15 @@ const errorMiddleware = (err, req, res, next) => {
     message: err.message || 'Internal Server Error',
   };
 
-  res.status(error.statusCode).json({ message: error.message });
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    res
+      .status(error.statusCode)
+      .json({ message: error.message, error: err, stack: err.stack });
+  }
+
+  if (process.env.NODE_ENV === 'PRODUCTION') {
+    res.status(error.statusCode).json({ message: error.message });
+  }
 };
 
 export { errorMiddleware };
