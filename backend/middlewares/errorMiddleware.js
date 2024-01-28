d9d633b49bb26a02b805951ac51c3db3fb5d0164
Hitesh <h08rajbhar@gmail.com>
2024-01-28 16:03:21 +0530

    validation and mongoose id error

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,8 +1,22 @@
+import ErrorHandler from '../utils/ErrorHandler.js';
+
 const errorMiddleware = (err, req, res, next) => {
   let error = {
     statusCode: err.statusCode || 500,
     message: err.message || 'Internal Server Error',
   };
+
+  // handle invalid mongoose ID error
+  if (err.name === 'CastError') {
+    const message = `Resource not Found. Invalid:${err.path}`;
+    error = new ErrorHandler(message, 404);
+  }
+
+  // handle validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorHandler(message, 400);
+  }
 
   if (process.env.NODE_ENV === 'DEVELOPMENT') {
     res
