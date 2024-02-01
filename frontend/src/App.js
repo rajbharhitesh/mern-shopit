57c410f756a7d358cc8b19fda092b913444aea60
Hitesh <h08rajbhar@gmail.com>
2024-02-01 17:26:50 +0530

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+
+      <div className="container">
+        <HomePage />
+      </div>
+
       <Footer />
     </div>
   );
