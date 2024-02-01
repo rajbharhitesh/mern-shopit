eefc0b94fa4f142c4fbbcb2fb5a7d5a64d443c43
Hitesh <h08rajbhar@gmail.com>
2024-02-01 18:26:42 +0530

    redux setup and get products redux endpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>
 );
