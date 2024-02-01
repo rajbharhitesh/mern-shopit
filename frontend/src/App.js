a9b733ac824f3b4df2e9e872e43dae5f3a1567d2
Hitesh <h08rajbhar@gmail.com>
2024-02-01 17:19:36 +0530

    header and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Footer from './components/layout/Footer';
-import Header from './components/layout/Header';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
