e458ac2221ef5bb52b6ddf2ec20e8fa533612d9c
Hitesh <h08rajbhar@gmail.com>
2024-02-02 21:06:47 +0530

    show user in header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,15 +1,19 @@
+import { useSelector } from 'react-redux';
 import { useGetMeQuery } from '../../redux/api/userApi';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const { data } = useGetMeQuery();
+  const { isLoading } = useGetMeQuery();
+
+  const { user } = useSelector((state) => state.auth);
 
   return (
     <nav className="navbar row">
       <div className="col-12 col-md-3 ps-5">
         <div className="navbar-brand">
-          <a href="/">
+          <Link to="/">
             <img src="/images/shopit_logo.png" alt="ShopIT Logo" />
-          </a>
+          </Link>
         </div>
       </div>
       <div className="col-12 col-md-6 mt-2 mt-md-0">
@@ -30,7 +34,7 @@ const Header = () => {
         </form>
       </div>
       <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-        <a href="/cart" style={{ textDecoration: 'none' }}>
+        <Link to="/cart" style={{ textDecoration: 'none' }}>
           <span id="cart" className="ms-3">
             {' '}
             Cart{' '}
@@ -38,55 +42,61 @@ const Header = () => {
           <span className="ms-1" id="cart_count">
             0
           </span>
-        </a>
+        </Link>
 
-        <div className="ms-4 dropdown">
-          <button
-            className="btn dropdown-toggle text-white"
-            type="button"
-            id="dropDownMenuButton"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <figure className="avatar avatar-nav">
-              <img
-                src="../images/default_avatar.jpg"
-                alt="User Avatar"
-                className="rounded-circle"
-              />
-            </figure>
-            <span>User</span>
-          </button>
-          <div
-            className="dropdown-menu w-100"
-            aria-labelledby="dropDownMenuButton"
-          >
-            <a className="dropdown-item" href="/admin/dashboard">
-              {' '}
-              Dashboard{' '}
-            </a>
+        {user ? (
+          <div className="ms-4 dropdown">
+            <button
+              className="btn dropdown-toggle text-white"
+              type="button"
+              id="dropDownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <figure className="avatar avatar-nav">
+                <img
+                  src={
+                    user.avatar ? user.avatar.url : '/images/default_avatar.jpg'
+                  }
+                  alt="User Avatar"
+                  className="rounded-circle"
+                />
+              </figure>
+              <span>{user.name}</span>
+            </button>
+            <div
+              className="dropdown-menu w-100"
+              aria-labelledby="dropDownMenuButton"
+            >
+              <Link className="dropdown-item" to="/admin/dashboard">
+                {' '}
+                Dashboard{' '}
+              </Link>
 
-            <a className="dropdown-item" href="/me/orders">
-              {' '}
-              Orders{' '}
-            </a>
+              <Link className="dropdown-item" to="/me/orders">
+                {' '}
+                Orders{' '}
+              </Link>
 
-            <a className="dropdown-item" href="/me/profile">
-              {' '}
-              Profile{' '}
-            </a>
+              <Link className="dropdown-item" to="/me/profile">
+                {' '}
+                Profile{' '}
+              </Link>
 
-            <a className="dropdown-item text-danger" href="/">
-              {' '}
-              Logout{' '}
-            </a>
+              <Link className="dropdown-item text-danger" to="/">
+                {' '}
+                Logout{' '}
+              </Link>
+            </div>
           </div>
-        </div>
-
-        <a href="/login" className="btn ms-4" id="login_btn">
-          {' '}
-          Login{' '}
-        </a>
+        ) : (
+          !isLoading && (
+            <Link to="/login" className="btn ms-4" id="login_btn">
+              {' '}
+              Login{' '}
+            </Link>
+          )
+        )}
       </div>
     </nav>
   );
