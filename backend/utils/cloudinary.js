4640cf6e267d1d797d6d2a91139e3226f9cb6f0b
Hitesh <h08rajbhar@gmail.com>
2024-02-03 16:53:48 +0530

    cloudinary upload setup

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -11,7 +11,7 @@ cloudinary.config({
 
 export const upload_file = (file, folder) => {
   return new Promise((resolve, reject) => {
-    cloudinary.v2.uploader.upload(
+    cloudinary.uploader.upload(
       file,
       (res) => {
         resolve({
@@ -25,7 +25,7 @@ export const upload_file = (file, folder) => {
 };
 
 export const delete_file = async (file) => {
-  const res = await cloudinary.v2.uploader.destroy(file);
+  const res = await cloudinary.uploader.destroy(file);
 
   if (res.result === 'ok') return true;
 };
