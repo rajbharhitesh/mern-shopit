802d91ff194414c550ccf629f34d4efe1f885ef6
Hitesh <h08rajbhar@gmail.com>
2024-02-08 11:32:46 +0530

    preparing for deploy

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,11 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import { fileURLToPath } from 'url';
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // handle uncaught exception
 process.on('uncaughtException', (err) => {
@@ -46,17 +50,11 @@ app.use('/api/v1', authRoute);
 app.use('/api/v1', orderRoute);
 app.use('/api/v1', paymentRoute);
 
-const __dirname = path.resolve();
-
 if (process.env.NODE_ENV === 'PRODUCTION') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
   });
 }
 
